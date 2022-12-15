933e9cd11f4011fee71a64263ae7cb1ee8f2e3c3
montedev0516 <montecristodev0516@gmail.com>
2022-12-16 08:54:01 +0900

    Profiel

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
 
 const PrivateRoute = ({
   component: Component,
-  auth: { isAuthenticated, loading }
+  auth: { isAuthenticated, loading },
 }) => {
   if (loading) return <Spinner />;
   if (isAuthenticated) return <Component />;
@@ -15,11 +15,11 @@ const PrivateRoute = ({
 };
 
 PrivateRoute.propTypes = {
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
