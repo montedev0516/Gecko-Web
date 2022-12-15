8847ca879a6aad28e09565ef2b2ba825fb644de8
montedev0516 <montecristodev0516@gmail.com>
2022-12-16 07:58:30 +0900

    Home Page

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,181 +7,94 @@ import AssetsTable from "../components/section/AssetsTable";
 const Home = () => {
   const { t } = useTranslation();
 
-  const AdditionalItems = () => {
-    return (
-      <>
-        <div className="max-w-[1000px] -mt-28 m-auto flex justify-between shadow-lg rounded-lg overflow-hidden ">
-          <div className="w-full">
-            <div className="px-6 py-3 flex justify-between items-center bg-[#E9E9E9]">
-              <p className="text-lg font-bold text-black">Trending</p>
-              <p className="text-md font-medium text-[#FF5665]">
-                View More {`>`}
+  return (
+    <>
+      <div className="bg-[#0B0B0F] pb-10">
+        <div className="n-container rounded-2xl bg-gradient-to-t from-[#1B1C23]/0 to-[#121318] p-10">
+          <div className="flex justify-between items-center gap-10">
+            <div className="sm:w-1/2">
+              <p className="text-white text-4xl font-bold leading-snug">
+                Today's Cryptocurrency Prices by{" "}
+                <span className="text-[#BA4DF9]">Football World Community</span>
               </p>
-            </div>
-            <div className="bg-white py-4">
-              <div className="border-r border-black/60 px-6">
-                <div className="flex justify-between items-center border-b pb-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-                <div className="flex justify-between items-center border-b py-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-                <div className="flex justify-between items-center pt-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="px-6 py-3 flex justify-between items-center bg-[#E9E9E9]">
-              <p className="text-lg font-bold text-black">Recently Added</p>
-              <p className="text-md font-medium text-[#FF5665]">
-                View More {`>`}
+              <p className="text-white text-lg mt-5 leading-relaxed">
+                The total crypto market volume over the last 24 hours is{" "}
+                <span className="text-[#BA4DF9]">$31.40B</span>, which makes a{" "}
+                <span className="text-[#BA4DF9]">29.97</span>% increase. The
+                total volume in DeFi is currently{" "}
+                <span className="text-[#BA4DF9]">$2.13B</span>,{" "}
+                <span className="text-[#BA4DF9]">6.78%</span> of the total
+                crypto market <span className="text-[#BA4DF9]">24-hour</span>{" "}
+                volume.
               </p>
+              <button className="text-white px-4 py-2 rounded-full bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9] shadow mt-6">
+                Start Trading
+              </button>
             </div>
-            <div className="bg-white py-4">
-              <div className="border-r border-black/60 px-6">
-                <div className="flex justify-between items-center border-b pb-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-                <div className="flex justify-between items-center border-b py-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-                <div className="flex justify-between items-center pt-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="px-6 py-3 flex justify-between items-center bg-[#E9E9E9]">
-              <p className="text-lg font-bold text-black">Top Community</p>
-              <p className="text-md font-medium text-[#FF5665]">
-                View More {`>`}
-              </p>
-            </div>
-            <div className="bg-white py-4">
-              <div className=" px-6">
-                <div className="flex justify-between items-center border-b pb-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-                <div className="flex justify-between items-center border-b py-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-                <div className="flex justify-between items-center pt-3 border-black/60">
-                  <div className="flex justify-start items-center gap-2">
-                    <img
-                      src="/img/tokens/bitcoin.png"
-                      alt=""
-                      className="h-8 w-8"
-                    />
-                    <p>Bitcoin</p>
-                    <p className="text-black/70 text-xs">BTC</p>
-                  </div>
-                  <p className="text-[#FF5665]">1.38%</p>
-                </div>
-              </div>
+            <div className="sm:w-1/2">
+              <img src="/img/prt.png" alt="" />
             </div>
           </div>
         </div>
-      </>
-    );
-  };
+        <div className="n-container p-10">
+          <div className="bg-[#121318] p-6 border border-[#23262F] rounded-lg">
+            <div className="flex justify-between items-center">
+              <p className="text-white font-bold text-2xl">
+                What Others Are{" "}
+                <span className="text-[#BA4DF9]">Searching</span>
+              </p>
+              <p className="text-[#BA4DF9] cursor-pointer">
+                View Most Popular Cryptocurrency
+              </p>
+            </div>
+            <div className="flex justify-between mt-6">
+              <div className="flex justify-start gap-2 items-center">
+                <img src="/img/tokens/waves.png" alt="" />
+                <p className="text-white text-lg font-bold">Waves</p>
+                <p className="text-[#BA4DF9] text-sm font-normal">WAVES</p>
+              </div>
+              <div className="flex justify-start gap-2 items-center">
+                <img src="/img/tokens/bitcoin.png" alt="" />
+                <p className="text-white text-lg font-bold">Bitcoin</p>
+                <p className="text-[#BA4DF9] text-sm font-normal">BTC</p>
+              </div>
+              <div className="flex justify-start gap-2 items-center">
+                <img src="/img/tokens/cake.png" alt="" />
+                <p className="text-white text-lg font-bold">PancakeSwap</p>
+                <p className="text-[#BA4DF9] text-sm font-normal">CAKE</p>
+              </div>
+              <div className="flex justify-start gap-2 items-center">
+                <img src="/img/tokens/ETH.png" alt="" />
+                <p className="text-white text-lg font-bold">Ethereum</p>
+                <p className="text-[#BA4DF9] text-sm font-normal">ETH</p>
+              </div>
+              <div className="flex justify-start gap-2 items-center">
+                <img src="/img/tokens/art.png" alt="" />
+                <p className="text-white text-lg font-bold">Ark</p>
+                <p className="text-[#BA4DF9] text-sm font-normal">Ark</p>
+              </div>
+            </div>
+          </div>
 
-  return (
-    <>
-      <div className="bg-primaryGray">
-        <Title
-          title={t(
-            "Today's Cryptocurrency Prices by Football World Communityt"
-          )}
-          subTitle={
-            <p className="text-white text-md text-center">
-              The total crypto market volume over the last 24 hours is{" "}
-              <span className="text-[#FF808A]"> $31.40B</span>, which makes a
-              <span className="text-[#FF808A]">29.97%</span> increase. The total
-              volume in DeFi is currently{" "}
-              <span className="text-[#FF808A]">$2.13B</span> ,{" "}
-              <span className="text-[#FF808A]">6.78%</span> of the total crypto
-              market <span className="text-[#FF808A]">24-hour</span> volume.
-            </p>
-          }
-          isBlock={true}
-        />
-        <AdditionalItems />
-        <AssetsTable />
+          <div className="flex justify-between items-center gap-10 mt-20">
+            <div className="sm:w-1/2">
+              <img src="/img/plt.png" alt="" />
+            </div>
+            <div className="sm:w-1/2">
+              <p className="text-white text-4xl font-bold leading-snug">
+                Be The First To Know About{" "}
+                <span className="text-[#BA4DF9]">Crypto News</span> Every Day
+              </p>
+              <p className="text-white text-lg mt-5 leading-relaxed">
+                Get crypto analysis, news and updates right to your inbox! Sign
+                up here so you don't miss a single newsletter.
+              </p>
+              <button className="text-white px-4 py-2 rounded-full bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9] shadow mt-6">
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
