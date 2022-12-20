6005d5304a5adedbc8c16b69fe54c8856876048e
montedev0516 <montecristodev0516@gmail.com>
2022-12-20 17:14:32 +0900

    Update List Token Page

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from "../actions/types";
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
--- src/views/listToken/StepSelector.js
+++ src/views/listToken/StepSelector.js
@@ -8,7 +8,7 @@ import StepLabel from "@mui/material/StepLabel";
 function StepSelector({ steps, activeStep, setActiveStep }) {
   return (
     <Box className="bg-gradient-to-r from-[#575A70]/20 to-[#575A70]/20 rounded-lg p-6 shadow-md">
-      <Stepper activeStep={activeStep} orientation="vertical">
+      {/* <Stepper activeStep={activeStep} orientation="vertical">
         {steps.map((step, index) => (
           <Step key={index}>
             <StepLabel>
@@ -19,7 +19,47 @@ function StepSelector({ steps, activeStep, setActiveStep }) {
             </StepLabel>
           </Step>
         ))}
-      </Stepper>
+      </Stepper> */}
+      {steps.map((step, key) => {
+        return (
+          <div key={key}>
+            {key !== 0 && (
+              <div className="flex justify-end mr-2">
+                <div
+                  className={`w-1 h-20 -my-4 z-0 ${
+                    key <= activeStep ? "bg-[#BA4DF9]" : "bg-[#54555A]"
+                  }`}
+                ></div>
+              </div>
+            )}
+            <div className="flex justify-end items-center gap-4">
+              <div className="text-end">
+                <p
+                  className={`${
+                    key === activeStep ? "text-white" : "text-white/40"
+                  }  text-lg font-bold`}
+                >
+                  {step.title}
+                </p>
+                <p
+                  className={`${
+                    key === activeStep ? "text-white" : "text-white/40"
+                  }  text-sm font-medium`}
+                >
+                  {step.description}
+                </p>
+              </div>
+              <div
+                className={`w-5 h-5 rounded-full z-10 ${
+                  key <= activeStep
+                    ? "bg-[#BA4DF9] border-2 border-[#4E2D67]"
+                    : "bg-[#54555A]"
+                } `}
+              ></div>
+            </div>
+          </div>
+        );
+      })}
     </Box>
   );
 }
