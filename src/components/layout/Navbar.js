beeb2bc2ce7201b5c282d064d11392cc1a22c3d3
montedev0516 <montecristodev0516@gmail.com>
2022-12-22 16:18:41 +0900

    Change fonts for all pages#18

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -51,8 +51,9 @@ const Navbar = () => {
               to={"/list-token"}
               onClick={() => localStorage.removeItem("list-token")}
             >
-              <button className="bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9] py-2 px-5 flex justify-center items-center rounded-full text-white font-medium">
-                + List Token
+              <button className="bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9] py-2 px-5 flex gap-1 justify-center items-center rounded-full text-white font-medium">
+                <img src="/img/list-token.png" alt="" className="h-6" />
+                Token Listing
               </button>
             </Link>
           </div>
