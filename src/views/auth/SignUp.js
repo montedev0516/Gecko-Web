b81f498094e9a727c09a99893066c4813338d57a
montedev0516 <montecristodev0516@gmail.com>
2022-12-26 13:00:57 +0900

    Update Sign Up responsive

--- src/views/auth/SignUp.js
+++ src/views/auth/SignUp.js
@@ -63,103 +63,106 @@ const Login = () => {
 
   return (
     <>
-      <div className="fixed top-0 overflow-auto left-0 w-screen min-h-screen authBg z-40">
-        <div className="sm:w-1/2 min-h-screen  h-full flex py-10 sm:p-0 justify-center items-center bg-gradient-to-t from-[#D38E4C]/20 to-[#1A1B23]/20">
-          <div className="max-w-[400px] w-11/12 h-full m-auto text-black">
-            <form onSubmit={onSubmit}>
-              <Link to="/">
-                <img
-                  src="/img/logo.png"
-                  alt=""
-                  className="w-32 h-32 m-auto sm:m-0"
-                />
-              </Link>
-              <p className="text-white font-bold text-4xl text-center sm:text-start">
-                Sign Into
-              </p>
-              <p className="text-white font-bold text-xl mt-3 text-center sm:text-start">
-                Your Account
-              </p>
-              <div className="mt-3">
-                <p className="text-white">{t("First Name")}</p>
-                <input
-                  type={"text"}
-                  placeholder="Enter first name"
-                  className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
-                  name="firstname"
-                  value={firstname}
-                  onChange={onChange}
-                  required
-                />
-              </div>
-              <div className="mt-3">
-                <p className="text-white">{t("Last Name")}</p>
-                <input
-                  type={"text"}
-                  placeholder="Enter last name"
-                  className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
-                  name="lastname"
-                  value={lastname}
-                  onChange={onChange}
-                  required
-                />
-              </div>
-              <div className="mt-3">
-                <p className="text-white">{t("Email")}</p>
-                <input
-                  type={"email"}
-                  placeholder="Enter email"
-                  className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
-                  name="email"
-                  value={email}
-                  onChange={onChange}
-                  required
-                />
-              </div>
-              <div className="mt-3">
-                <p className="text-white">{t("Password")}</p>
-                <input
-                  type={"password"}
-                  placeholder="Enter password"
-                  className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
-                  name="password"
-                  value={password}
-                  onChange={onChange}
-                  minLength="6"
-                  required
-                />
-              </div>
-              <div className="flex justify-end mt-4">
-                {/* <p className="text-sm items-center text-white flex gap-1">
+      {/* authBg */}
+      <div className="fixed top-0 overflow-auto left-0 w-screen h-screen authBg z-40">
+        <div className="">
+          <div className="sm:w-1/2 min-h-screen h-full flex py-10 justify-center items-center bg-gradient-to-t from-[#D38E4C]/20 to-[#1A1B23]/20">
+            <div className="max-w-[400px] w-11/12 h-full m-auto text-black">
+              <form onSubmit={onSubmit}>
+                <Link to="/">
+                  <img
+                    src="/img/logo.png"
+                    alt=""
+                    className="w-32 h-32 m-auto sm:m-0"
+                  />
+                </Link>
+                <p className="text-white font-bold text-4xl text-center sm:text-start">
+                  Sign Into
+                </p>
+                <p className="text-white font-bold text-xl mt-3 text-center sm:text-start">
+                  Your Account
+                </p>
+                <div className="mt-3">
+                  <p className="text-white">{t("First Name")}</p>
+                  <input
+                    type={"text"}
+                    placeholder="Enter first name"
+                    className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
+                    name="firstname"
+                    value={firstname}
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+                <div className="mt-3">
+                  <p className="text-white">{t("Last Name")}</p>
+                  <input
+                    type={"text"}
+                    placeholder="Enter last name"
+                    className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
+                    name="lastname"
+                    value={lastname}
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+                <div className="mt-3">
+                  <p className="text-white">{t("Email")}</p>
+                  <input
+                    type={"email"}
+                    placeholder="Enter email"
+                    className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+                <div className="mt-3">
+                  <p className="text-white">{t("Password")}</p>
+                  <input
+                    type={"password"}
+                    placeholder="Enter password"
+                    className="py-2 px-4 w-full mt-2 rounded outline-none bg-gradient-to-t from-[#575A70] to-[#575A70] text-white"
+                    name="password"
+                    value={password}
+                    onChange={onChange}
+                    minLength="6"
+                    required
+                  />
+                </div>
+                <div className="flex justify-end mt-4">
+                  {/* <p className="text-sm items-center text-white flex gap-1">
                 <input type={"checkbox"} /> {t("Remember Me")}
               </p> */}
-                <p className="text-sm text-[#BA4DF9]">Forgot Password?</p>
+                  <p className="text-sm text-[#BA4DF9]">Forgot Password?</p>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9] text-white mt-5 h-12 rounded"
+                >
+                  {t("SIGN UP")}
+                </button>
+              </form>
+              <div className="mt-4 flex items-center text-white justify-between">
+                <hr className="w-2/5" />
+                <p className="font-bold">{t("OR")}</p>
+                <hr className="w-2/5" />
               </div>
               <button
-                type="submit"
-                className="w-full bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9] text-white mt-5 h-12 rounded"
+                onClick={onGoogleLogin}
+                className="w-full flex justify-center items-center gap-3 bg-gradient-to-r from-[#575A70]/40 to-[#575A70]/50 text-white mt-3 h-12 rounded"
               >
-                {t("SIGN UP")}
+                <img src="/img/google.png" alt="" className="w-8" />
+                {t("Login in with Google")}
               </button>
-            </form>
-            <div className="mt-4 flex items-center text-white justify-between">
-              <hr className="w-2/5" />
-              <p className="font-bold">{t("OR")}</p>
-              <hr className="w-2/5" />
+              <p className="text-white text-center mt-5">
+                {t("Don't have a account")}? &nbsp;
+                <Link to={"/login"}>
+                  <span className="text-[#BA4DF9]">{t("Sign In")}</span>
+                </Link>
+              </p>
             </div>
-            <button
-              onClick={onGoogleLogin}
-              className="w-full flex justify-center items-center gap-3 bg-gradient-to-r from-[#575A70]/40 to-[#575A70]/50 text-white mt-3 h-12 rounded"
-            >
-              <img src="/img/google.png" alt="" className="w-8" />
-              {t("Login in with Google")}
-            </button>
-            <p className="text-white text-center mt-5">
-              {t("Don't have a account")}? &nbsp;
-              <Link to={"/login"}>
-                <span className="text-[#BA4DF9]">{t("Sign In")}</span>
-              </Link>
-            </p>
           </div>
         </div>
       </div>
