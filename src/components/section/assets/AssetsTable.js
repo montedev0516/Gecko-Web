b08c2d0740b28d34251a6ae91c741282a330ca44
montedev0516 <montecristodev0516@gmail.com>
2023-02-10 05:17:44 +0900

    Update Table

--- src/components/section/assets/AssetsTable.js
+++ src/components/section/assets/AssetsTable.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useToken from "../../../hook/useToken";
 import Table from "../../common/table/Table";
-import formatNumber from "../../../utils/formatNumber";
+import { formatNumber } from "../../../utils";
 
 function AssetsTable() {
   const navigate = useNavigate();
@@ -23,16 +23,12 @@ function AssetsTable() {
   }, []);
 
   const columns = [
-    { name: "#", selector: (row, key) => key + 1, width: '3%' },
+    { name: "#", selector: (row, key) => key + 1, width: "3%" },
     {
       name: "Name",
       selector: (row) => (
         <div className="flex justify-start items-center gap-3 py-2">
-          <img
-            src={row.logo}
-            alt=""
-            className="h-8 w-8 rounded-full"
-          />
+          <img src={row.logo} alt="" className="h-8 w-8 rounded-full" />
           <div>
             <p className="text-white font-bold text-md">{row.name}</p>
             <p className="text-[#9B9B9B] text-sm">{row.symbol}</p>
@@ -44,11 +40,51 @@ function AssetsTable() {
     { name: "Price", selector: (row) => row.quoteLatest.price.toFixed(6) },
     {
       name: "1h%",
-      selector: (row) => <p className={row.quoteLatest.percent_change_1h > 0 ? "text-[#16C784]" : "text-[#FF0000]"}>{row.quoteLatest.percent_change_1h}%</p>,
+      selector: (row) => (
+        <p
+          className={
+            row.quoteLatest.percent_change_1h > 0
+              ? "text-[#16C784]"
+              : "text-[#FF0000]"
+          }
+        >
+          {row.quoteLatest.percent_change_1h}%
+        </p>
+      ),
     },
-    { name: "24h%", selector: (row) => <p className={row.quoteLatest.percent_change_24h > 0 ? "text-[#16C784]" : "text-[#FF0000]"}>{row.quoteLatest.percent_change_24h}%</p> },
-    { name: "7d%", selector: (row) => <p className={row.quoteLatest.percent_change_7d > 0 ? "text-[#16C784]" : "text-[#FF0000]"}>{row.quoteLatest.percent_change_7d}%</p> },
-    { name: "Market Cap", width: "12%", selector: (row) => formatNumber(row.quoteLatest.market_cap) },
+    {
+      name: "24h%",
+      selector: (row) => (
+        <p
+          className={
+            row.quoteLatest.percent_change_24h > 0
+              ? "text-[#16C784]"
+              : "text-[#FF0000]"
+          }
+        >
+          {row.quoteLatest.percent_change_24h}%
+        </p>
+      ),
+    },
+    {
+      name: "7d%",
+      selector: (row) => (
+        <p
+          className={
+            row.quoteLatest.percent_change_7d > 0
+              ? "text-[#16C784]"
+              : "text-[#FF0000]"
+          }
+        >
+          {row.quoteLatest.percent_change_7d}%
+        </p>
+      ),
+    },
+    {
+      name: "Market Cap",
+      width: "12%",
+      selector: (row) => formatNumber(row.quoteLatest.market_cap),
+    },
     {
       name: "Volume(24h)",
       width: "12%",
@@ -61,7 +97,11 @@ function AssetsTable() {
     {
       name: "Circulating Supply",
       width: "15%",
-      selector: (row) => {return `${formatNumber(row.quoteLatest.circulating_supply)} ${row.symbol}`},
+      selector: (row) => {
+        return `${formatNumber(row.quoteLatest.circulating_supply)} ${
+          row.symbol
+        }`;
+      },
     },
     {
       name: "Last 7 Days",
