713c1c3ca86c1c94c96a4918c02dfc1dea5d1ccf
montedev0516 <montecristodev0516@gmail.com>
2023-02-10 05:27:10 +0900

    Update Token Detail

--- src/components/section/tokenDetail/Chart.js
+++ src/components/section/tokenDetail/Chart.js
@@ -1,31 +1,7 @@
 import React from "react";
 
 function Chart(props) {
-  const types = [
-    "Overview",
-    "Markets",
-    "Historical Data",
-    "News",
-    "Price Estimates",
-    "More Info",
-  ];
-
-  return (
-    <div>
-      <div className="itemBg5 rounded-full w-max p-2 border-[#23262F] dark:border">
-        {types.map((row, key) => {
-          return (
-            <button
-              key={key}
-              className="text-[#8E8E8E] hover:text-white px-6 py-2 rounded-full hover:bg-gradient-to-r from-[#5B46DF] to-[#BA4DF9]"
-            >
-              {row}
-            </button>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Chart;
